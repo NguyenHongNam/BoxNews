9d350447946d78cca60e03f9843da59150ffda52
kieulan1912 <kieulan826@gmail.com>
2023-07-23 11:42:03 +0700

    update theo category

--- BoxNews/Service/PostService.cs
+++ BoxNews/Service/PostService.cs
@@ -37,16 +37,27 @@ namespace BoxNews.Service{
         public void UpdatePost(UpdatePostViewModel post)
         {
             var existingPost = _context.Posts.Find(post.PostID);
-
+            post.Categories = _context.Categories.ToList();
+            var categoryName = post.Categories.Find(x => x.CategoryID == post.CategoryID).CategoryName.ToString();
             if (existingPost != null)
             {
                 existingPost.Title = post.Title;
                 existingPost.Author = post.Author;
                 existingPost.Content = post.Content;
+                // if (image != null && image.Length > 0)
+                // {
+                //     var fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                //     var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+                //     using (var stream = new FileStream(filePath, FileMode.Create))
+                //     {
+                //         image.CopyTo(stream);
+                //     }
+                //     post.ImgSrc = "wwwroot/images/" + fileName;
+                // }
                 // existingPost.ImgSrc = post.ImgSrc;
                 existingPost.Status = post.Status;
-                // existingPost.CategoryID = post.CategoryID;
-
+                existingPost.CategoryID = post.CategoryID;
+                existingPost.CategoryName = categoryName;
                 _context.SaveChanges();
             }
         }
